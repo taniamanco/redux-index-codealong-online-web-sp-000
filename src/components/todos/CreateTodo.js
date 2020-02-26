b9bbe7146aeb7e113c523e01258bf7f71109ba5c
Tania Manco <belalov.tania16@gmail.com>
2020-02-26 21:19:06 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -10,10 +10,13 @@ class CreateTodo extends Component {
     };
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.addTodo(this.state)
-  }
+ handleSubmit = event => {
+  event.preventDefault();
+  this.props.addTodo(this.state)
+  this.setState({
+    text: '',
+  })
+}
 
   handleChange(event) {
     this.setState({
